b1e257b55f9e0a2b9bf2837eb4434e018f5db77b
Abdul Rahiman AT <31490398+abdulrahimanat@users.noreply.github.com>
2020-12-22 21:02:56 +0530

    Update cloudtrail-org.sh

--- cloudtrail-org.sh
+++ cloudtrail-org.sh
@@ -7,24 +7,17 @@ https://github.com/abdulrahimanat/cloud-formation/blob/main/org-taril.bucket.yam
 
 # Step 3 Gather the S3 bucket name that we created earlier using https://github.com/abdulrahimanat/cloud-formation/blob/main/org-taril.bucket.yaml.
 
-aws cloudformation describe-stack-resources --stack-name ####replace stack_name here #### <span class="pl-k">|</span> \
-  grep Physical | \
-  grep s3bucket
+aws cloudformation describe-stack-resources --stack-name ####ReplaceStackName#### grep Physical | grep s3bucket
   
  # step 4 we have to enable all organizational features
   aws organizations enable-all-features
   
  # step 5 enable service access for cloudtrail.
- aws organizations enable-aws-service-access \
-  --service-principal cloudtrail.amazonaws.com
+ aws organizations enable-aws-service-access --service-principal cloudtrail.amazonaws.com
   
   # step 6 wlcreate the actual trail, Replace the bucket name with the one you created using above CFT
   
-  aws cloudtrail create-trail \
-  --name org-trail \
-  --s3-bucket-name cloudtrails3bucket-s3bucket-78is3f3s2eqj \
-  --is-organization-trail \
-  --is-multi-region-trail
+  aws cloudtrail create-trail --name org-trail --s3-bucket-name ####ReplaceBucketName#### --is-organization-trail --is-multi-region-trail
   
   
   
